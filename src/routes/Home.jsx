53413c643e22e6904fd43befa6f5c82c43f953d3
Cameron Williams <cswilliams112004@gmail.com>
2024-04-22 22:50:11 -0400

    first commit

---
+++ src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <>
+      <div className="page-home">This is the home page.</div>
+    </>
+  );
+};
+export default Home;
